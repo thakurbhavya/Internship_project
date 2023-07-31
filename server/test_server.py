63377a937b5a3339ac5a60144d6a802425730ea8
thakurbhavya <87276414+thakurbhavya@users.noreply.github.com>
2023-07-31 18:21:56 +0530

    Update test_server.py

--- server/test_server.py
+++ server/test_server.py
@@ -1,1 +1,10 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
 
+@app.route('/hello')
+def hello():
+    return "Hi this is a test API"
+
+if __name__ == "__main__":
+    app.run(port=5000)
+    
